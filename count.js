367840bf0d5ba7e7168c87f64092d4ff4ab0e23c
CraZY <ayang879@gmail.com>
2019-03-11 18:29:57 +0800

    Create count.js

---
+++ count.js
@@ -0,0 +1,14 @@
+let arr = require("./originArr");
+
+let counter = new Array();
+let res = [];
+for (let item of arr) {
+	counter[item] = (counter[item] || 0) + 1;
+}
+counter.forEach((e, index) => {
+	if (e) {
+		res = res.concat(new Array(e).fill(index));
+	}
+});
+
+console.log(JSON.stringify(res));
