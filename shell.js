2a70f322029ccf8627c4d2a6d0ad02b1569a49d7
CraZY <ayang879@gmail.com>
2019-03-11 18:31:07 +0800

    Create shell.js

---
+++ shell.js
@@ -0,0 +1,15 @@
+let arr = require("./originArr");
+let swap = require("./swap");
+
+let initGap = Math.ceil(arr.length / 2);
+while (initGap >= 1) {
+	for (let index = 0; index + initGap < arr.length; index++) {
+		if (arr[index] > arr[index + initGap]) {
+			let [a, b] = swap(arr[index], arr[index + initGap]);
+			arr[index] = a;
+			arr[index + initGap] = b;
+		}
+	}
+	initGap -= 2;
+}
+console.log(JSON.stringify(arr));
