797293acb786b721158cc368dc21e1d882079866
CraZY <ayang879@gmail.com>
2019-06-02 15:50:08 +0800

    Update writable.js

--- stream.js/writable.js
+++ stream.js/writable.js
@@ -1,11 +1,49 @@
-const { Writable } = require("stream");
+const { Writable, Readable } = require("stream");
 
-class myWritable extends Writable {
-	_write() {}
+let ws = new Writable({
+	write: function(chunk, encoding, next) {
+		console.log(`in writable:${chunk.toString()}`);
+		next();
+	},
+	highWaterMark: 1
+});
+ws.write("-2");
+ws.write("-1");
+let index = 0;
+let rs = new Readable({
+	read: function(size) {
+		if (index > 100) {
+			this.push(null);
+		} else {
+			console.log(`in readable:${index}`);
+			// process.nextTick(() => {
+			this.push(`${index}`);
+			index++;
+			// });
+		}
+	},
+	highWaterMark: 20
+});
+rs.on("data", function(chunk) {
+	if (ws.writable) {
+		// process.nextTick(() => {
+		let ret = ws.write(chunk);
+		if (!ret) {
+			rs.pause();
+		}
+		// });
+	} else {
+		rs.pause();
+	}
+});
 
-	_writev() {}
-}
-
-// let assert = require("assert");
-
-// assert.equal
+rs.on("end", function() {
+	ws.end();
+});
+ws.on("finish", function() {
+	ws.destroy();
+	rs.destroy();
+});
+ws.on("drain", function() {
+	rs.resume();
+});
